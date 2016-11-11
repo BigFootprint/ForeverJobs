3b054afabb8ad6f23b33e93e6518c021ce1a0da7
quanmin.li <quanmin.li@dianping.com>
2016-11-11 22:59:00 +0800

    修改

--- MyP/JobsConstants.h
+++ MyP/JobsConstants.h
@@ -1,16 +1,15 @@
 //
-//  NSConstant.h
+//  JobsConstantss.h
 //  MyP
 //
 //  Created by 李全民 on 16/11/11.
 //  Copyright © 2016年 李全民. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#ifndef JobsConstantss_h
+#define JobsConstantss_h
 
 // Color
 #define ANDROID_BLUE [UIColor colorWithRed:(48.0 / 255.0) green:(198 / 255.0) blue:(255 / 255.0) alpha:1.0]
 
-@interface JobsConstants : NSObject
-
-@end
+#endif /* JobsConstantss_h */
