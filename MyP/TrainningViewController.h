b033aa1eb59cae6ef5354ec510f238fc3fd86a5c
quanmin.li <quanmin.li@dianping.com>
2016-11-11 16:29:59 +0800

    优化训练营的数据和布局

--- MyP/TrainningViewController.h
+++ MyP/TrainningViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface TrainningViewController : UIViewController
+@interface TrainningViewController : UIViewController 
 
 @end
 
--- MyP/TrainningViewController.m
+++ MyP/TrainningViewController.m
@@ -9,13 +9,14 @@
 #import "TrainningViewController.h"
 #import "UIButtonViewController.h"
 
-@interface TrainningViewController ()
+@interface TrainningViewController ()<UITableViewDataSource, UITableViewDelegate>
 @property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) NSArray<NSArray<NSString *> *> *data;
+@property (nonatomic, strong) NSArray<NSString *> *dataGroup;
+
 -(void)initView;
--(void)buttonClick;
 -(NSArray *)getKnowledgeArray;
 +(int)getStatusHeight;
-
 @end
 
 @implementation TrainningViewController
@@ -23,37 +24,99 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    _data = [self getKnowledgeArray];
+    _dataGroup = [self getKnowledgeGroup];
     [self initView];
-    NSLog(@"%@", @"Home-viewDidLoad");
 }
 
 -(void)viewWillAppear:(BOOL)animated{
-    NSLog(@"%@", @"Home-viewWillAppear");
     self.tabBarController.navigationItem.title = @"训练营";
 }
 
 -(void)initView {
-    int statusHeight = [TrainningViewController getStatusHeight];
     // 添加 ScrollView
     CGSize screenSize = [[UIScreen mainScreen] bounds].size;
-    _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 44 + statusHeight, screenSize.width, screenSize.height - 44 - 49 - statusHeight)];
-    
-    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"bg_sky"]];
-    
-    [_tableView setBackgroundView:imageView];
+    _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, screenSize.width, screenSize.height - 49)];
+
+    [_tableView setDataSource:self];
+    _tableView.delegate = self;
+    _tableView.separatorInset = UIEdgeInsetsMake(0, 0, 0, 0); // 设置端距，这里表示separator离左边和右边均80像素
     [self.view addSubview:_tableView];
-    
-    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 100, 50)];
-    button.center = CGPointMake(screenSize.width/2, screenSize.height/2);
-    [button setTitle:@"测试" forState:UIControlStateNormal];
-    [button setBackgroundColor:[UIColor orangeColor]];
-    [button addTarget:self action:@selector(buttonClick) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:button];
 }
 
 -(void)buttonClick{
     UIButtonViewController *nextController = [[UIButtonViewController alloc] init];
     [self.tabBarController.navigationController pushViewController:nextController animated:YES];
+}
+
+#pragma mark - 数据源方法
+#pragma mark 返回分组数
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return _data.count;
+}
+
+#pragma mark 返回每组行数
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    NSArray *array = _data[section];
+    return array.count;
+}
+
+#pragma mark返回每行的单元格
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    //NSIndexPath是一个结构体，记录了组和行信息
+    UITableViewCell *cell=[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
+    cell.textLabel.text=_data[indexPath.section][indexPath.row];
+    return cell;
+}
+
+#pragma mark 返回每组头标题名称
+-(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+    return _dataGroup[section];
+}
+
+#pragma mark 设置分组标题内容高度
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    return 40;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    CGSize screenSize = [[UIScreen mainScreen] bounds].size;
+    
+    // create the button object
+    UILabel *headerLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+    headerLabel.backgroundColor = [UIColor clearColor];
+    headerLabel.opaque = NO;
+    headerLabel.textAlignment = NSTextAlignmentCenter;
+    headerLabel.textColor = [UIColor colorWithRed:(54.0 / 255.0) green:(100.0 / 255.0) blue:(139.0 / 255.0) alpha:1.0];
+    headerLabel.highlightedTextColor = [UIColor blackColor];
+    headerLabel.font = [UIFont boldSystemFontOfSize:20];
+    headerLabel.frame = CGRectMake(0, 0, screenSize.height, 40);
+    
+    NSString *headerText = [[NSString alloc]initWithFormat:@"%@  %@  %@", @"====", _dataGroup[section], @"===="];
+    [headerLabel setText:headerText];
+    return headerLabel;
+}
+
+#pragma mark 点击行
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSLog(@"%@", _data[indexPath.section][indexPath.row]);
+    switch (indexPath.section) {
+        case 0:
+            switch (indexPath.row) {
+                case 0:{// UIButton
+                        UIButtonViewController *buttonVc = [[UIButtonViewController alloc] init];
+                        [self.tabBarController.navigationController pushViewController:buttonVc animated:YES];
+                    }
+                    break;
+                default:
+                    break;
+            }
+            break;
+        default:
+            break;
+    }
 }
 
 +(int)getStatusHeight{
@@ -62,37 +125,19 @@ CGRect rectStatus = [[UIApplication sharedApplication] statusBarFrame];
 }
 
 -(NSArray *)getKnowledgeArray{
-    return @[@"UIButton", @"UILabel", @"UIImageView", @"UITextField", @"UIAlertView", @"UILabel", @"UIImageView", @"UITextField", @"UIAlertView"];
+    NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"data" ofType:@"plist"];
+    NSMutableDictionary *plistData = [[NSMutableDictionary alloc] initWithContentsOfFile:plistPath];
+    return [plistData objectForKey:@"knowledge"];
+}
+
+-(NSArray *)getKnowledgeGroup{
+    NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"data" ofType:@"plist"];
+    NSMutableDictionary *plistData = [[NSMutableDictionary alloc] initWithContentsOfFile:plistPath];
+    return [plistData objectForKey:@"knowledgegroup"];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-    NSLog(@"%@", @"Home-didReceiveMemoryWarning");
-}
-
--(void)loadView{
-    [super loadView];
-    NSLog(@"%@", @"Home-loadView");
-}
-
--(void)viewWillDisappear:(BOOL)animated{
-    [super viewWillDisappear:animated];
-    NSLog(@"%@", @"Home-viewWillDisappear");
-}
-
--(void)viewDidAppear:(BOOL)animated{
-    [super viewDidAppear:animated];
-    NSLog(@"%@", @"Home-viewDidAppear");
-}
-
--(void)viewWillLayoutSubviews{
-    [super viewWillLayoutSubviews];
-    NSLog(@"%@", @"Home-viewWillLayoutSubviews");
-}
-
--(void)viewDidLayoutSubviews{
-    [super viewDidLayoutSubviews];
-    NSLog(@"%@", @"Home-viewDidLayoutSubviews");
 }
 @end
