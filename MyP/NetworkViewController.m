06e85fb754e58e33705d2a246559510161a0ee90
quanmin.li <quanmin.li@dianping.com>
2016-11-20 20:43:14 +0800

    添加AFNetworking相关实验

--- MyP/NetworkViewController.m
+++ MyP/NetworkViewController.m
@@ -10,11 +10,13 @@
 #import "NetworkViewController.h"
 #import "JobsConstants.h"
 #import "Masonry.h"
+#import "AFNetworking.h"
 
 @interface NetworkViewController ()
 @property(nonatomic, strong) dispatch_queue_t networkQueue;
 @property(nonatomic, strong) dispatch_queue_t mainQueue;
--(void)senRequest:(id)button;
+-(void)sendNSURLSessionRequest:(id)button;
+-(void)sendAFNetworingRequest:(id)button;
 @end
 
 @implementation NetworkViewController
@@ -26,13 +28,21 @@
     // Do any additional setup after loading the view.
     [self.view setBackgroundColor:ANDROID_BLUE];
     
-    UIButton *networkButton = [[UIButton alloc] init];
-    [networkButton setBackgroundColor:[UIColor blackColor]];
-    [networkButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    [networkButton setTitle:@"发送请求" forState:UIControlStateNormal];
-    [networkButton setTitleColor:[UIColor colorWithRed:200/255.0f green:200/255.0f blue:200/255.0f alpha:1.0f] forState:UIControlStateHighlighted];
-    [networkButton addTarget:self action:@selector(senRequest:) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:networkButton];
+    UIButton *nSURLSessionNetworkButton = [[UIButton alloc] init];
+    [nSURLSessionNetworkButton setBackgroundColor:[UIColor blackColor]];
+    [nSURLSessionNetworkButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [nSURLSessionNetworkButton setTitle:@"NSURLSession" forState:UIControlStateNormal];
+    [nSURLSessionNetworkButton setTitleColor:[UIColor colorWithRed:200/255.0f green:200/255.0f blue:200/255.0f alpha:1.0f] forState:UIControlStateHighlighted];
+    [nSURLSessionNetworkButton addTarget:self action:@selector(sendNSURLSessionRequest:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:nSURLSessionNetworkButton];
+    
+    UIButton *aFNetworkingNetworkButton = [[UIButton alloc] init];
+    [aFNetworkingNetworkButton setBackgroundColor:[UIColor blackColor]];
+    [aFNetworkingNetworkButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [aFNetworkingNetworkButton setTitle:@"AFNetworking" forState:UIControlStateNormal];
+    [aFNetworkingNetworkButton setTitleColor:[UIColor colorWithRed:200/255.0f green:200/255.0f blue:200/255.0f alpha:1.0f] forState:UIControlStateHighlighted];
+    [aFNetworkingNetworkButton addTarget:self action:@selector(sendAFNetworingRequest:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:aFNetworkingNetworkButton];
     
     responseLable = [[UILabel alloc] init];
     responseLable.backgroundColor = [UIColor whiteColor];
@@ -40,16 +50,25 @@
     responseLable.numberOfLines = 0;
     [self.view addSubview:responseLable];
     
-    [networkButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.width.mas_equalTo(self.view.mas_width);
+    [nSURLSessionNetworkButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.right.mas_equalTo(self.view.mas_centerX).offset(-5);
+        make.left.mas_equalTo(self.view.mas_left);
         make.top.mas_equalTo(self.view.mas_top).offset(80);
         make.height.mas_equalTo(40);
     }];
+    
+    [aFNetworkingNetworkButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(nSURLSessionNetworkButton.mas_right).offset(10);
+        make.top.mas_equalTo(self.view.mas_top).offset(80);
+        make.height.mas_equalTo(40);
+        make.right.mas_equalTo(self.view.mas_right);
+    }];
+    
     [responseLable mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.mas_equalTo(self.view.mas_left).offset(10);
         make.right.mas_equalTo(self.view.mas_right).offset(-10);
         make.bottom.mas_equalTo(self.view.mas_bottom).offset(-10);
-        make.top.mas_equalTo(networkButton.mas_bottom).offset(10);
+        make.top.mas_equalTo(nSURLSessionNetworkButton.mas_bottom).offset(10);
     }];
     
     _networkQueue = dispatch_queue_create("com.footprint.MyP.Network", DISPATCH_QUEUE_CONCURRENT);
@@ -60,8 +79,8 @@
     // TODO CFNetwork
 }
 
--(void)senRequest:(id)button{
-    responseLable.text = @"请求中...";
+-(void)sendNSURLSessionRequest:(id)button{
+    responseLable.text = @"NSURLSession 请求中...";
     dispatch_async(_networkQueue, ^{
         // 快捷方式获得session对象
         NSURLSession *session = [NSURLSession sharedSession];
@@ -78,6 +97,30 @@
                                         }];
         [task resume];
     });
+}
+
+-(void)sendAFNetworingRequest:(id)button{
+    responseLable.text = @"AFNetworking 请求中...";
+    // 启动系统风火轮
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+    NSString *domainStr = @"http://httpbin.org/headers";
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    //以get的形式提交，只需要将上面的请求地址给GET做参数就可以
+    [manager GET:domainStr parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        // 隐藏系统风火轮
+        [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+        
+        //json解析
+        NSString *response = [NSString stringWithFormat:@"%@", [NSJSONSerialization JSONObjectWithData:responseObject options:kNilOptions error:nil]];
+        self.responseLable.text = response;
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        
+        // 解析失败隐藏系统风火轮(可以打印error.userInfo查看错误信息)
+        [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+        
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
