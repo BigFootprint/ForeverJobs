4e3ef8c215be66b1d034a9574910b75e365c9bb1
quanmin.li <quanmin.li@dianping.com>
2016-11-21 19:59:19 +0800

    添加CoreData实验

---
+++ MyP/CoreDataViewController.h
@@ -0,0 +1,13 @@
+//
+//  CoreDataViewController.h
+//  MyP
+//
+//  Created by 李全民 on 16/11/21.
+//  Copyright © 2016年 李全民. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoreDataViewController : UIViewController
+
+@end
---
+++ MyP/CoreDataViewController.m
@@ -0,0 +1,42 @@
+//
+//  CoreDataViewController.m
+//  MyP
+//
+//  Created by 李全民 on 16/11/21.
+//  Copyright © 2016年 李全民. All rights reserved.
+//
+
+#import "CoreDataViewController.h"
+#import "Masonry.h"
+#import "JobsConstants.h"
+
+@interface CoreDataViewController ()
+
+@end
+
+@implementation CoreDataViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [self.view setBackgroundColor:ANDROID_BLUE];
+    
+    Teacher
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
