7d612f7eb2b9bb327d98cf80e37814b304bb2140
quanmin.li <quanmin.li@dianping.com>
2017-01-04 18:16:39 +0800

    添加可变色的 UIImage

---
+++ MyP/UIImage+Tint.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+Tint.h
+//  MyP
+//
+//  Created by 李全民 on 17/1/4.
+//  Copyright © 2017年 李全民. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Tint)
+- (UIImage *) imageWithTintColor:(UIColor *)tintColor;
+@end
---
+++ MyP/UIImage+Tint.m
@@ -0,0 +1,28 @@
+//
+//  UIImage+Tint.m
+//  MyP
+//
+//  Created by 李全民 on 17/1/4.
+//  Copyright © 2017年 李全民. All rights reserved.
+//
+
+#import "UIImage+Tint.h"
+
+@implementation UIImage (Tint)
+- (UIImage *) imageWithTintColor:(UIColor *)tintColor
+{
+    //We want to keep alpha, set opaque to NO; Use 0.0f for scale to use the scale factor of the device’s main screen.
+    UIGraphicsBeginImageContextWithOptions(self.size, NO, 0.0f);
+    [tintColor setFill];
+    CGRect bounds = CGRectMake(0, 0, self.size.width, self.size.height);
+    UIRectFill(bounds);
+    
+    //Draw the tinted image in context
+    [self drawInRect:bounds blendMode:kCGBlendModeDestinationIn alpha:1.0f];
+    
+    UIImage *tintedImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return tintedImage;
+}
+@end
