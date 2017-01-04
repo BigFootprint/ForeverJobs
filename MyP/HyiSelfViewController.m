351de4a30f3eeed648bd9efe6955a0c2882668c8
quanmin.li <quanmin.li@dianping.com>
2017-01-04 21:30:27 +0800

    删除不必要代码

--- MyP/HyiSelfViewController.m
+++ MyP/HyiSelfViewController.m
@@ -8,7 +8,7 @@
 
 #import "HyiSelfViewController.h"
 
-@interface HyiSelfViewController ()<UINavigationControllerDelegate>
+@interface HyiSelfViewController ()
 
 @end
 
@@ -17,8 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    // 设置导航控制器的代理为self
-    self.navigationController.delegate = self;
 }
 
 -(void)viewWillAppear:(BOOL)animated {
@@ -27,14 +25,6 @@
 
 -(void)viewWillDisappear:(BOOL)animated {
     [self.navigationController setNavigationBarHidden:NO animated:NO];
-}
-
-#pragma mark - UINavigationControllerDelegate
-// 将要显示控制器
-- (void)navigationController:(UINavigationController *)navigationController willShowViewController:(UIViewController *)viewController animated:(BOOL)animated {
-    // 判断要显示的控制器是否是自己
-    BOOL isShowHomePage = [viewController isKindOfClass:[self class]];
-    [self.navigationController setNavigationBarHidden:isShowHomePage animated:YES];
 }
 
 @end
